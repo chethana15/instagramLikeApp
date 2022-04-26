7c125398c64296b53f220e2792861e36b1856643
Chethana N P <chethana.np@cumulations.com>
2022-04-26 16:57:53 +0530

    Initial Commit

---
+++ instagramLikeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagramLikeApp
+//
+//  Created by Cumulations Technologies Private Limited on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
